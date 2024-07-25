acfc8df04db54aa1f97354345b59e1ef8de3d3bc
Aditya Dey <deyaditya2004@gmail.com>
2024-07-25 13:11:31 +0530

    added thirdJavaFile

---
+++ thirdJavaFile.java
@@ -0,0 +1,6 @@
+public class thirdJavaFile {
+    public static void main(String[] args) {
+        System.out.println("Hello, thirdJavaFile.class!");
+        System.out.println("Hello VS CODE");
+    }
+}
